2ce19106135eb12ff6671e0e682ed2f5c960e495
Philipp Defner <mail@notmyhostna.me>
2022-05-01 21:25:34 +0200

    Add rate limiting trigger

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	b64 "encoding/base64"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"net/http/httputil"
@@ -34,6 +35,7 @@ func main() {
 		return
 	}
 	ticker := time.NewTicker(time.Duration(minutes) * time.Minute)
+	var counter int
 	for ; true; <-ticker.C {
 		fmt.Printf("➡️  try to refresh token at %s using refresh_token %s...\n", time.Now().Format(time.RFC3339), refreshToken[:10])
 		params := url.Values{}
@@ -43,6 +45,10 @@ func main() {
 
 		client := &http.Client{}
 		req, err := http.NewRequest("POST", "https://api.twitter.com/2/oauth2/token", body)
+		if err != nil {
+			fmt.Println("err", err)
+			continue
+		}
 
 		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 		req.Header.Add("Authorization", fmt.Sprintf("Basic %s", b64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%s:%s", clientID, clientSecret)))))
@@ -76,6 +82,48 @@ func main() {
 		fmt.Println("✅ successfully refreshed token. new tokens stored for next call.")
 		fmt.Println("   access_token:", accessToken)
 		fmt.Println("   refresh_token:", refreshToken)
+		counter++
+
+		if counter == 2 {
+			triggered, err := triggerRateLimit(client, accessToken)
+			if err != nil {
+				fmt.Println("err", err)
+				continue
+			}
+			if triggered {
+				fmt.Println("triggered rate limit!")
+				continue
+			}
+		}
+	}
+}
+
+func triggerRateLimit(c *http.Client, accessToken string) (bool, error) {
+	var reqCounter int
+	for {
+		if accessToken == "" {
+			return false, errors.New("access token can't be empty")
+		}
+		req, err := http.NewRequest("GET", "https://api.twitter.com/2/tweets?ids=1261326399320715264,1278347468690915330", nil)
+		if err != nil {
+			return false, err
+		}
+		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+		req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", accessToken))
+		resp, err := c.Do(req)
+		if err != nil {
+			return false, err
+		}
+		if resp.StatusCode == http.StatusTooManyRequests {
+			return true, nil
+		}
+		if resp.StatusCode == http.StatusOK {
+			fmt.Println("fetching tweets. retry count: ", reqCounter)
+		} else {
+			return false, fmt.Errorf("unexpected status code: %d, wanted 200", resp.StatusCode)
+		}
+		reqCounter++
+		continue
 	}
 }
 
